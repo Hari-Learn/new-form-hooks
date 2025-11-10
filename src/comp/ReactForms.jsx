e4edc4257c51fbd3ee5c73cc9c0b6fc8432bdf0a
Hari-repo <hariharanuk810@gmail.com>
2025-11-10 15:14:23 +0530

    React-hook-form with validation added for login forms

--- src/comp/ReactForms.jsx
+++ src/comp/ReactForms.jsx
@@ -1,9 +1,53 @@
 import React from "react";
+import { useForm } from "react-hook-form";
 
 const ReactForms = () => {
+    const {register,handleSubmit, reset,watch,formState:{errors}} = useForm()
+
+    const onSubmitForm = (data) => {
+        console.table(data)
+    }
+    const allValues = watch();
+    console.log(allValues)
+
+    const handleReset = () => {
+        reset();
+    }
+
     return(
         <>
             <h3>React-hook-forms</h3>
+            <form onSubmit={handleSubmit(onSubmitForm)} >
+                <input type="text" placeholder="Enter Name"
+                {...register('name',{required:"Name is required",
+                    minLength:{
+                        value:4,
+                        message:"Name should contain minimum 4 charcter"
+                    }
+                })} style={{marginLeft:"0px",marginTop:"10px",marginRight:"5px"}} />
+
+                {errors.name && (<span>{errors.name.message}</span>)}
+                    <br/>
+                <input type="text" placeholder="Enter Email"
+                {...register('email',{
+                    required:"Email is required",
+                    pattern: {
+                        value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+                        message: "Enter a valid email address"
+                        }})} style={{marginLeft:"0px",marginTop:"10px",marginRight:"5px"}}  />
+
+                {errors.email && (<span>{errors.email.message}</span>)}
+                        <br/>
+                <input type="submit" 
+                style={{margin:"10px",marginLeft:"0px",backgroundColor:"#3498db", padding:"10px",fontFamily:"sans-serif",color:"#fff",fontWeight:"bold",border:"none"}}   />
+
+                <button type="button" onClick={handleReset} 
+                style={{margin:"10px",backgroundColor:"#3498db", padding:"10px",fontFamily:"sans-serif",color:"#fff",fontWeight:"bold",border:"none"}}  >
+                    Reset
+                </button>
+
+            </form>
+
         </>
     )
 }
